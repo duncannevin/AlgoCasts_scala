0f42b5f80796d7128d1a7316143eaa0fb29e978d
duncan nevin <duncan.nevin@gmail.com>
2020-04-14 20:17:15 -0400

    tree, levelwidth

--- src/main/scala/tree/Tree.scala
+++ src/main/scala/tree/Tree.scala
@@ -11,6 +11,17 @@ class Tree[T] {
 
   def traverseDF(cb: T => AnyVal): Unit = traverse(isBF = false, cb, List(root))
 
+  def levelWidth: List[Int] = levelWidth(List(root, null), List(0))
+
+  @scala.annotation.tailrec
+  private def levelWidth(slate: List[Node[T]], widths: List[Int]): List[Int] = slate match {
+    case head::Nil if head == null => widths
+    case head::tail => head match {
+      case null => levelWidth(tail :+ null, widths :+ 0)
+      case _ => levelWidth(tail ::: head.getChildren, widths.init :+ widths.last + 1)
+    }
+  }
+
   @scala.annotation.tailrec
   private def traverse(isBF: Boolean, cb: T => AnyVal, slate: List[Node[T]]): Unit = slate match {
     case Nil =>
--- src/test/scala/tree/TreeSpec.scala
+++ src/test/scala/tree/TreeSpec.scala
@@ -36,4 +36,31 @@ class TreeSpec extends AnyFlatSpec with Matchers {
 
     numbers should equal (List(1, 2, 3, 4))
   }
+
+  "Tree" should "get level widths" in {
+    val t = new Tree[Int]
+    t.setRoot(0)
+    val root = t.getRoot
+
+    root.add(1)
+    root.add(2)
+    root.add(3)
+    root.getChildren.head.add(4)
+    root.getChildren(2).add(5)
+
+    t.levelWidth should equal (List(1, 3, 2))
+  }
+
+  "Tree" should "get level widths of nodes at widest point" in {
+    val t = new Tree[Int]
+    t.setRoot(0)
+    val root = t.getRoot
+
+    root.add(1)
+    root.getChildren.head.add(2)
+    root.getChildren.head.add(3)
+    root.getChildren.head.getChildren.head.add(4)
+
+    t.levelWidth should equal (List(1, 1, 2, 1))
+  }
 }
